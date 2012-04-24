fba25edb0321a1e9b286084e6bfef77920bccc65
Christan Kulpa <chris@almostintelligent.de>
2012-04-24 19:58:07 +0200

    Added distribution process zu build.gradle

---
+++ hydra-api/src/main/java/org/gethydrated/hydra/api/Dummy.java
@@ -0,0 +1,11 @@
+package org.gethydrated.hydra.api;
+
+/**
+ * This is a dummy to prevent javadoc from
+ * failing because of no classes being found.
+ * @author chris
+ *
+ */
+public class Dummy {
+
+}
---
+++ hydra-cli/src/main/java/org/gethydrated/hydra/cli/Dummy.java
@@ -0,0 +1,11 @@
+package org.gethydrated.hydra.cli;
+
+/**
+ * This is a dummy to prevent javadoc from
+ * failing because of no classes being found.
+ * @author chris
+ *
+ */
+public class Dummy {
+
+}
