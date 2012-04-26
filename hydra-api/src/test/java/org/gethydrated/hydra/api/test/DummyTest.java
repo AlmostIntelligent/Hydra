8a4df43509a6d0bb589f14f73f8cd26b2f2c2d48
Christian Kulpa <chris@almostintelligent.de>
2012-04-26 16:25:53 +0200

    Improved build to run test when building dist Added logback dependencies Added logback-test.xml to all subprojects Added dummy classes / test

---
+++ hydra-api/src/test/java/org/gethydrated/hydra/api/test/DummyTest.java
@@ -0,0 +1,17 @@
+package org.gethydrated.hydra.api.test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DummyTest {
+
+	@Test
+	public void dummyTest() {
+		Logger logger = LoggerFactory.getLogger(DummyTest.class);
+		logger.info("dummy");
+		assertTrue(true);
+	}
+}
---
+++ hydra-launcher/src/test/java/org/gethydrated/hydra/api/test/DummyTest.java
@@ -0,0 +1,13 @@
+package org.gethydrated.hydra.api.test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class DummyTest {
+
+	@Test
+	public void dummyTest() {
+		assertTrue(true);
+	}
+}
