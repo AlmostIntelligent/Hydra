b53a51273243d1cbde194e5734acb5ae1f342446
Christian Kulpa <chris@almostintelligent.de>
2012-05-03 14:57:57 +0200

    Added method to get hydra base directory

--- hydra-launcher/src/main/java/org/gethydrated/hydra/launcher/Launcher.java
+++ hydra-launcher/src/main/java/org/gethydrated/hydra/launcher/Launcher.java
@@ -1,14 +1,31 @@
 package org.gethydrated.hydra.launcher;
 
+import java.net.URISyntaxException;
+
 public class Launcher {
 
+	private static String workingDir;
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		if(setWorkingDirectory()) {
+			System.out.println(workingDir);
+		}
 	}
 
+	private static boolean setWorkingDirectory() {
+		try {
+			String path =Launcher.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath();
+			path = path.substring(0, path.lastIndexOf('/'));
+			workingDir = path.substring(0, path.lastIndexOf('/')+1);
+			return true;
+		} catch (URISyntaxException e) {
+			System.err.println("Could not set working directory");
+			return false;
+		}
+	}
 	
 	public static void printUsage() {
 		System.out.println("Hydra Usage:");
