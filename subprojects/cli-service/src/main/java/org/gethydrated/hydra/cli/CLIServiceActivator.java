206fc596f8c18a48be9ec77f3cbc87c4d7ebace5
Christan Kulpa <chris@almostintelligent.de>
2012-05-07 21:41:37 +0200

    [Add] cli service/activator class

---
+++ subprojects/cli-service/src/main/java/org/gethydrated/hydra/cli/CLIServiceActivator.java
@@ -0,0 +1,20 @@
+package org.gethydrated.hydra.cli;
+
+import org.gethydrated.hydra.api.service.ServiceActivator;
+import org.gethydrated.hydra.api.service.ServiceContext;
+
+public class CLIServiceActivator implements ServiceActivator {
+
+	@Override
+	public void start(ServiceContext context) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stop(ServiceContext context) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
