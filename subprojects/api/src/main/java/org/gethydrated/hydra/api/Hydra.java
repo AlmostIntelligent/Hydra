dcf37a92e1b0f0142d5abef4974fc2fc7a3ac0bd
Christan Kulpa <chris@almostintelligent.de>
2012-05-09 01:35:33 +0200

    [change] Api structure

--- subprojects/api/src/main/java/org/gethydrated/hydra/api/Hydra.java
+++ subprojects/api/src/main/java/org/gethydrated/hydra/api/Hydra.java
@@ -5,9 +5,15 @@ package org.gethydrated.hydra.api;
  * @author chris
  * @since 0.1.0
  */
-public interface Hydra {
+public interface Hydra extends HydraApi {
 
+	/**
+	 * Starts the Hydra.
+	 */
 	public void start();
 
+	/**
+	 * Stops the Hydra.
+	 */
 	public void stop();
 }
---
+++ subprojects/api/src/main/java/org/gethydrated/hydra/api/HydraException.java
@@ -0,0 +1,14 @@
+package org.gethydrated.hydra.api;
+
+public class HydraException extends Exception {
+
+	public HydraException(Throwable e) {
+		super(e);
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 9050861266290634703L;
+
+}
