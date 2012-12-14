901ca5fef9954f8931a18672fb48e044199c2fb1
Christian Kulpa <chris@almostintelligent.de>
2012-12-14 12:56:40 +0100

    Deleted MailBox.java - Git rename error

--- subprojects/actors/src/main/java/org/gethydrated/hydra/actors/mailbox/MailBox.java
+++ subprojects/actors/src/main/java/org/gethydrated/hydra/actors/mailbox/MailBox.java
@@ -1,13 +1,28 @@
 package org.gethydrated.hydra.actors.mailbox;
 
-
 public interface Mailbox {
-
-    Message take() throws InterruptedException;
 
     Message poll();
 
+    Message pollSystem();
+
     void offer(Message m);
 
+    void offerSystem(Message m);
+
     boolean hasMessages();
+
+    boolean hasSystemMessages();
+
+    boolean isScheduled();
+
+    void setScheduled(boolean state);
+
+    boolean isSuspended();
+
+    void setSuspended(boolean state);
+
+    void close();
+
+    boolean isClosed();
 }
