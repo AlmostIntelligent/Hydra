e368c51d885a207d5cd40dd5bdfc0951202f9106
Christian Kulpa <chris@almostintelligent.de>
2013-01-29 18:50:18 +0100

    Changed XMLConfigurationReader to use hydra.util.XMLReader Deployed new xsd schema files

---
+++ subprojects/config/src/main/java/org/gethydrated/hydra/config/files/ConfigurationParser.java
@@ -0,0 +1,26 @@
+package org.gethydrated.hydra.config.files;
+
+import org.gethydrated.hydra.api.configuration.Configuration;
+import org.gethydrated.hydra.util.xml.XMLParser;
+import org.w3c.dom.Element;
+
+/**
+ *
+ */
+public class ConfigurationParser implements XMLParser<Configuration> {
+
+    @Override
+    public Configuration getResult() {
+        return null;
+    }
+
+    @Override
+    public void startElement(Element element) {
+
+    }
+
+    @Override
+    public void endElement(Element element) {
+
+    }
+}
